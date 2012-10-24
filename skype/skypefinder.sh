7d548e11bb351aa521c70ead2a807e55aac9b52d
Jim Zajkowski <jamesez@umich.edu>
2012-10-24 13:34:39 -0400

    More logic

--- skype/skypefinder.sh
+++ skype/skypefinder.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-NEWLOC=`curl -I "http://www.skype.com/go/getskype-macosx" 2>/dev/null | grep Location | sed 's/Location: //' | tr -d '\r'`
+URL=`curl -I "http://www.skype.com/go/getskype-macosx" 2>/dev/null | grep Location | sed 's/Location: //' | tr -d '\r'`
+count=0
 
-if [ "x${NEWLOC}" != "x" ]; then
-        echo "${NEWLOC}"
-fi
+while [ $count -lt 10 ] && ! echo ${URL} | grep -q .dmg; do
+  count=$(($count + 1))
+  sleep 2
+  URL=`curl -I "http://www.skype.com/go/getskype-macosx" 2>/dev/null | grep Location | sed 's/Location: //' | tr -d '\r'`
+done
+
+echo "${URL}"
