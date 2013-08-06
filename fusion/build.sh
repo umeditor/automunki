8cd135205c17324191569f3ef19b5458f444eff5
Patrick McNeal <patrick.mcneal@gmail.com>
2013-08-06 15:06:44 -0400

    Correctly create installer

--- fusion/build.sh
+++ fusion/build.sh
@@ -23,9 +23,15 @@ cp "${url}" app.dmg
 # Mount
 mountpoint=`hdiutil attach -mountrandom /tmp -nobrowse app.dmg | awk '/private\/tmp/ { print $NF } '`
 
-# Copy app for modification
+# Copy deploy package
 mkdir build-root
-ditto "${mountpoint}/VMware Fusion.app" "build-root/VMware Fusion.app"
+ditto "${mountpoint}/VMware Fusion.app/Contents/Library/Deploy VMware Fusion.mpkg" "build-root/Deploy VMware Fusion.mpkg"
+
+# Add the serial number
+echo "key = ${serial}" >> "build-root/Deploy VMware Fusion.mpkg/Contents/00Fusion_Deployment_Items/Deploy.ini"
+
+# Copy app for modification
+ditto "${mountpoint}/VMware Fusion.app" "build-root/Deploy VMware Fusion.mpkg/Contents/00Fusion_Deployment_Items/VMware Fusion.app"
 
 # unmount
 hdiutil detach "${mountpoint}"
@@ -33,18 +39,25 @@ hdiutil detach "${mountpoint}"
 # remove original dmg
 rm app.dmg
 
-# Add the serial number
-echo "key = ${serial}" >> "build-root/VMware Fusion.app/Contents/Library/Deploy VMware Fusion.mpkg/Contents/00Fusion_Deployment_Items/Deploy.ini"
-
 # Create disk image from modified app
-hdiutil create -volname "VMware Fusion" -srcfolder "build-root/VMware Fusion.app" -ov -format UDZO app.dmg
+hdiutil create -volname "VMware Fusion" -srcfolder "build-root/Deploy VMware Fusion.mpkg" -ov -format UDZO app.dmg
 
-# Build pkginfo
+# Find all the appropriate apps, etc, and then turn that into -f's
+key_files=`find build-root -name '*.app' -or -name '*.plugin' -or -name '*.prefPane' -or -name '*component' | sed 's/ /\\\\ /g; s/^/-f /' | paste -s -d ' ' -`
+
+# plist prefers full paths
 plist=`pwd`/app.plist
-/usr/local/munki/makepkginfo -m go-w -g admin -o root app.dmg > "${plist}"
+versionplist="`pwd`/build-root/Deploy VMware Fusion.mpkg/Contents/00Fusion_Deployment_Items/VMware Fusion.app/Contents/Info.plist"
 
-# Obtain version info
-version=`defaults read "${plist}" version`
+# Build pkginfo (this is done through an echo to expand key_files)
+echo /usr/local/munki/makepkginfo -m go-w -g admin -o root app.dmg ${key_files} | /bin/bash > ${plist}
+
+# Fix paths
+perl -p -i -e 's/build-root\/Deploy VMware Fusion.mpkg\/Contents\/00Fusion_Deployment_Items/Applications/' ${plist}
+
+# Obtain and set version
+version=`defaults read "${versionplist}" CFBundleShortVersionString`
+defaults write "${plist}" version "${version}"
 
 # Change path and other details in the plist
 defaults write "${plist}" installer_item_location "${prefix}-${version}${suffix}.dmg"
